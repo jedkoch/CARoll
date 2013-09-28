0ae0607cc69091e211053b2797035b10b492a192
jed <help@eelele.com>
2013-09-27 17:59:36 -0700

    Added auto level function during boot
    in first 4 seconds after boot device calibrates incase not mounted
    level. still needs improvement. currently using servo steps to cal
    instead of actual device position.

--- _3_Axis_Accelerometer_servo_control_6_01.ino
+++ _3_Axis_Accelerometer_servo_control_6_01.ino
@@ -1,9 +1,9 @@
-//_3_Axis_Accelerometer_servo_control_6_01
+//_3_Axis_Accelerometer_servo_control_6_02
 //Code for RCV3AXIS accelerometer board released as
 //California Roll by eel electronics
 //eelele.com
 //working rev for The California Roll - 3AXIS RC-CAR v15.7
-
+//6.02 added auto level during 4 second boot
 
 //Include Libraries
 #include <Servo.h>
@@ -40,14 +40,54 @@ Servo fServoR;
 Servo bServoL;
 Servo bServoR;
 
-//unused - possible future timing vars
-unsigned long time = millis();
-unsigned long previousMillis = 0;
-int interval = 2;//how often the servos update in milliseconds
+//timing vars for auto cal during startup
+volatile unsigned long time = millis();
+volatile unsigned long tapTimeout = 4000;
+
+void calMODE()//this calibrates the position of the device during startup
+{
+  time = millis();
+  while(time < tapTimeout)
+  {
+    accel.update();
+    xpos = map(accel.x(), -1024, 1024, -90, 90);  
+    ypos = map(accel.y(), -1024, 1024, -90, 90);
+    xNEUTRAL = (90+xpos);
+    yNEUTRAL = (ypos);
+    time = millis();
+  }
+  return;
+}
+
+void moveMODE()
+{
+  while(true)
+  {
+    accel.update();
+    xpos = map(accel.x(), -1024, 1024, -90, 90);//the accel.y and accel.x can be switched so the boards  
+    ypos = map(accel.y(), -1024, 1024, -90, 90);// can be installed lengthwise on the vehicle
+    zpos = map(accel.z(), -1024, 1024, 30, -10);
+    xpos=kalmanCalculateX(xpos,newRate,looptime);
+    ypos=kalmanCalculateY(ypos,newRate,looptime);
+    zpos=kalmanCalculateZ(zpos,newRate,looptime);
+    int fR = (xNEUTRAL+xpos+ypos-yNEUTRAL);
+    int fL = (xNEUTRAL-xpos+ypos-yNEUTRAL);
+    int bR = (xNEUTRAL+xpos-ypos+yNEUTRAL);
+    int bL = (xNEUTRAL-xpos-ypos+yNEUTRAL);
+    fR = constrain(fR,0,180);
+    fL = constrain(fL,0,180);
+    bR = constrain(bR,0,180);
+    bL = constrain(bL,0,180);
+    fServoR.write(fR);
+    fServoL.write(fL);
+    bServoR.write(bR);
+    bServoL.write(bL);
+  }  
+}
 
 void setup()
 {
-  previousMillis = millis();
+  time = millis();
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(9, OUTPUT);
@@ -57,55 +97,17 @@ void setup()
   digitalWrite(9, HIGH);
   digitalWrite(10, HIGH);
   accel.setI2CAddr(0x1C); //alt 0x1D if SAO pulled HIGH // default is 0x1C
-  accel.dataMode(true, 2); //enable highRes 10bit, 2g range [2g,4g,8g]
   //I2c.begin();
   fServoR.attach(servoPin_fr);
   fServoL.attach(servoPin_fl);
   bServoR.attach(servoPin_br);
   bServoL.attach(servoPin_bl);
-  //  accel.update();
-  //  xNEUTRAL = accel.y(); 
-  //  yNEUTRAL = accel.x();
 }
 
 void loop()
 {
-  accel.update();
-  //xpos = map(accel.x(), -1024, 1024, -512, 512); 
-  //ypos = map(accel.y(), -1024, 1024, -512, 512);
-  //zpos = map(accel.z(), -1024, 1024, -512, 512);
-  xpos = map(accel.x(), -1024, 1024, -90, 90);//the accel.y and accel.x can be switched so the boards  
-  ypos = map(accel.y(), -1024, 1024, -90, 90);// can be installed lengthwise on the vehicle
-  zpos = map(accel.z(), -1024, 1024, 30, -10);
-  xpos=kalmanCalculateX(xpos,newRate,looptime);
-  ypos=kalmanCalculateY(ypos,newRate,looptime);
-  zpos=kalmanCalculateZ(zpos,newRate,looptime);
-  int fR = (xNEUTRAL+xpos+ypos+zpos);
-  int fL = (xNEUTRAL-xpos+ypos+zpos);
-  int bR = (xNEUTRAL+xpos-ypos+zpos);
-  int bL = (xNEUTRAL-xpos-ypos+zpos);
-  fR = constrain(fR,0,180);
-  fL = constrain(fL,0,180);
-  bR = constrain(bR,0,180);
-  bL = constrain(bL,0,180);
-  fServoR.write(fR);
-  fServoL.write(fL);
-  bServoR.write(bR);
-  bServoL.write(bL);
-
-  //delay(100);
+  accel.dataMode(true, 2);
+  calMODE();//first two seconds after powerup are waiting for tap and calibrate
+  accel.dataMode(true, 2); //enable highRes 10bit, 2g range [2g,4g,8g]
+  moveMODE();//ready for
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
